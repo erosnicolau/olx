01a75793f5347076192e74aae2928d72c4d14e3f
Eros Nicolau <eros.nicolau@gmail.com>
2019-12-12 14:53:16 +0200

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+# olx
+Add show-hide functionality for selected cars on olx.ro
+
+## How it works
+The script adds a small 🙈 icon to all the ads (both in the listing page and in the car page).
+Clicking that icon "disables" the listing item, keeping it in the browser memory as disabled, until you click it again
+
+## Installation
+Install by clicking [here](https://github.com/erosnicolau/olx/raw/master/olx.user.js)
---
+++ olx.user.js
@@ -0,0 +1,83 @@
+// ==UserScript==
+// @name         OLX
+// @namespace    https://www.olx.ro/
+// @version      0.1
+// @description  Hide unwanted ads
+// @author       Eros Nicolau
+// @match        https://www.olx.ro/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict'
+    console.clear()
+    let ls = window.localStorage
+
+    // Add the hide icons to the listing ads
+    let cadAds = document.querySelectorAll('tr.wrap')
+    cadAds.length > 0 && [...cadAds].map(e => {
+        let id = e.querySelector('table').getAttribute('data-id')
+        addMonkey(e, id)
+        if (e.querySelectorAll('.autovitro_label').length ==1) {
+            let currentCars = JSON.parse(ls.getItem("Hidden Cars")) || [];
+            !currentCars.includes(id) && ls.setItem("Hidden Cars", JSON.stringify([id, ...currentCars]))
+        }
+    })
+
+    // Check to see if you're on a car page; If so, then add the hide icon to the Details box
+    try { if (criteoEvents.product.events.viewItem.item) addMonkey(document.querySelector('.offer-sidebar'), criteoEvents.product.events.viewItem.item) } catch (e){}
+
+    // Look into localStorage and hide all the cars that match the hidden cars list
+    let currentCars = JSON.parse(ls.getItem("Hidden Cars")) || [];
+    if (currentCars.length > 0) {
+        currentCars.map(c => {
+            let that = document.querySelector('[data-hiddenid="'+c+'"]')
+            that && toggleCar(that, [...that.classList].includes('hiddenButton'))
+        })
+    }
+
+    function addMonkey(e, id){
+        let tlink = document.createElement('a'),
+            link = e.parentNode.insertBefore(tlink, e);
+        link.setAttribute('href','#')
+        link.setAttribute('data-hiddenid', id);
+        link.classList.add('hideMe')
+        link.innerText = '🙈'
+        link.addEventListener("click", function(el){
+            el.preventDefault()
+            let that = el.target
+            toggleCar(that, [...that.classList].includes('hiddenButton'))
+        }, false);
+    }
+
+    function toggleCar(el, hidden){
+        let id = el.getAttribute('data-hiddenid')
+        let currentCars = JSON.parse(ls.getItem("Hidden Cars")) || [];
+        if(hidden == true) {
+            el.nextSibling.classList.remove("hiddenAd")
+            el.classList.remove('hiddenButton')
+            ls.setItem("Hidden Cars", JSON.stringify(currentCars.filter(e => e != id)))
+        } else {
+            el.nextSibling.classList.add("hiddenAd")
+            el.classList.add("hiddenButton")
+            !currentCars.includes(id) && ls.setItem("Hidden Cars", JSON.stringify([id, ...currentCars]))
+        }
+    }
+
+    // Add custom CSS
+    var head = document.head || document.getElementsByTagName("head")[0],
+        style = document.createElement("style"),
+        customCSS = ''
+    head.appendChild(style)
+    style.type = "text/css"
+    customCSS += ".hideMe {font-size: 24px; cursor: pointer; float: right; transform: translate(16px, 20px); z-index: 6; position: relative}"
+    customCSS += ".hideMe:hover {opacity: .5}"
+    customCSS += ".hidden, .adcontainer-tr, .a, .rightBranding, [id^=Crt], #bnr {display: none !important}"
+    customCSS += ".hiddenAd {height: 45px; min-height: unset !important; overflow: hidden; opacity: 0.15; display: block}"
+    customCSS += ".hiddenAd td {display: block}"
+    customCSS += ".hiddenAd .tags, .hiddenAd h3, .hiddenAd .favorite-box {display: none;}"
+    customCSS += ".hiddenButton {opacity: 0.15}"
+    if (style.styleSheet) style.styleSheet.cssText = customCSS
+    else style.appendChild(document.createTextNode(customCSS))
+
+})();
